0cd445d8b780b97c1a68629230e202382fdf9d62
jgarcia9007 <jgarcia9007@outlook.com>
2026-03-03 09:50:35 +0100

    Fix NEXTAUTH_URL to use http instead of https

--- deploy_sgflota.sh
+++ deploy_sgflota.sh
@@ -50,7 +50,7 @@ fi
 echo "Configurando archivo .env..."
 cat > .env <<EOF
 DATABASE_URL="postgresql://jorge:J*rg3.90@localhost:5432/urban-rentals?schema=public"
-NEXTAUTH_URL="https://urban-rentals.es"
+NEXTAUTH_URL="http://urban-rentals.es"
 NEXTAUTH_SECRET="$(openssl rand -hex 32)"
 EOF
 
